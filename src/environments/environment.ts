563fa91823419b63d0756ade585b461097413820
roottool <operation.versatile.roottool@gmail.com>
2020-10-20 22:01:21 +0000

    Upgraded some packages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyANfWX1ccuVhQnndqGdezvSvGt5HxOxx4Y',
+    authDomain: 'orgasound-6b59e.firebaseapp.com',
+    databaseURL: 'https://orgasound-6b59e.firebaseio.com',
+    projectId: 'orgasound-6b59e',
+    storageBucket: 'orgasound-6b59e.appspot.com',
+    messagingSenderId: '1035739920924'
   }
 };
 
